3fb8463f6cf2a8b9ab8401e83bc2a0a28cdcdf56
kennedyreiS2 <kennedyreis@bemol.com.br>
2024-01-23 21:47:53 -0400

    chore: adjust file folder

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ def train_model() -> None:
     model = YOLO('yolov8n-cls.pt')
 
     # Train the model
-    model.train(data='./Classificacao-de-imagens-1', epochs=2, imgsz=32)
+    model.train(data='../data/Classificacao-de-imagens-1', epochs=2, imgsz=32)
 
 if __name__ == "__main__":
     train_model()
